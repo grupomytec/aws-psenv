25204de081867f7e930ede4d4efacd7b67b8f752
Matheus Alves <theuves@gmail.com>
2020-12-05 11:57:35 -0400

    feat(psenv): adiciona opção --value-from

--- aws-psenv.sh
+++ aws-psenv.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
 
-ssm_path=$1
+is_value_from=0
 
-test -z $ssm_path \
-  && echo "[error] missing path." \
-  && exit 1
+for argument in $@
+do
+  if [ $argument = "--value-from" ]
+  then
+    is_value_from=1
+  else
+    ssm_path=$argument
+  fi
+done
+
+if [ -z $ssm_path ]
+then
+  echo "[error] Missing path."
+  echo "Usage:"
+  echo "  --value-from  Get the ARN to use with valueFrom."
+  exit 1
+fi
 
 which aws > /dev/null \
   || (echo "[error] 'aws-cli' is not installed." && exit 1)
@@ -12,14 +26,21 @@ which aws > /dev/null \
 which jq > /dev/null \
   || (echo "[error] 'jq' is not installed." && exit 1)
 
-aws ssm get-parameters-by-path --with-decryption --path $ssm_path |
-  jq --raw-output -M '
-    [(
-      .Parameters[]
-        | del(.Type, .Version, .LastModifiedDate, .ARN, .DataType)
-        | to_entries
-        | map_values(.key = (.key | ascii_downcase))
-        | map_values(.value = if .key == "name" then (.value | split("/"))[-1] else .value end)
-        | from_entries
-    )]
-  '
+output=$(aws ssm get-parameters-by-path --with-decryption --path $ssm_path | jq --raw-output -M '
+  [(
+    .Parameters[]
+      | del(.Type, .Version, .LastModifiedDate, .DataType)
+      | to_entries
+      | map_values(.key = (.key | ascii_downcase))
+      | map_values(.key = if .key == "arn" then "valueFrom" else .key end)
+      | map_values(.value = if .key == "name" then (.value | split("/"))[-1] else .value end)
+      | from_entries
+  )]
+')
+
+if [ $is_value_from = "1" ]
+then
+  echo $output | jq -M '[.[] | del(.value)]'
+else
+  echo $output | jq -M '[.[] | del(.valueFrom)]'
+fi
